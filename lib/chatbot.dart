57098629e23bdd11477a173206debae36195b642
Vict0r2055 <luyasmpanza@gmail.com>
2023-07-26 11:55:25 +0200

    another life to live  another life to live

--- lib/chatbot.dart
+++ lib/chatbot.dart
@@ -1,8 +1,5 @@
 // Import required packages
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 const String OPENAI_API_URL =
     'https://api.openai.com/v1/engines/text-davinci-003/completions';
@@ -15,41 +12,6 @@ class ChatScreen extends StatefulWidget {
 class _ChatScreenState extends State<ChatScreen> {
   final TextEditingController _messageController = TextEditingController();
   final List<String> _messages = [];
-
-  Future<void> _sendMessage(String message) async {
-    // Load the API key from .env before using it
-    await dotenv.load();
-    String apiKey = dotenv.env['OPENAI_API_KEY'] ?? "";
-
-    setState(() {
-      _messages.add('You: $message');
-    });
-
-    final response = await http.post(
-      Uri.parse(OPENAI_API_URL),
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer $apiKey',
-      },
-      body: jsonEncode({
-        'prompt': message,
-        'max_tokens': 100,
-        'temperature': 0.7,
-      }),
-    );
-
-    if (response.statusCode == 200) {
-      final data = jsonDecode(response.body);
-      final reply = data['choices'][0]['text'].trim();
-      setState(() {
-        _messages.add('Chatbot: $reply');
-      });
-    } else {
-      setState(() {
-        _messages.add('Chatbot: Something went wrong.');
-      });
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -81,7 +43,6 @@ class _ChatScreenState extends State<ChatScreen> {
                   onPressed: () {
                     String message = _messageController.text.trim();
                     if (message.isNotEmpty) {
-                      _sendMessage(message);
                       _messageController.clear();
                     }
                   },
