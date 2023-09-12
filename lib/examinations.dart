2812de93d25ffab86721c08433f16860316b68a2
Vict0r2055 <luyasmpanza@gmail.com>
2023-09-12 21:04:14 +0200

    exams appended

--- lib/examinations.dart
+++ lib/examinations.dart
@@ -3,8 +3,60 @@ import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 import 'package:campus_buddy/bottom_nav.dart';
 
-class ExaminationsPage extends StatelessWidget {
-  const ExaminationsPage({Key? key}) : super(key: key);
+class Event {
+  final String title;
+  final DateTime date;
+
+  Event(this.title, this.date);
+}
+
+List<Event> _getEventsForDay(DateTime day) {
+  return events[day] ?? [];
+}
+
+// Example map of events (replace with your actual data)
+Map<DateTime, List<Event>> events = {
+  DateTime.utc(2023, 8, 17): [
+    Event('Event 1', DateTime.utc(2023, 8, 17)),
+    Event('Event 2', DateTime.utc(2023, 8, 17)),
+  ],
+  DateTime.utc(2023, 8, 18): [
+    Event('Event 3', DateTime.utc(2023, 8, 18)),
+  ],
+  // ... more entries for other days
+};
+
+class ExaminationsPage extends StatefulWidget {
+  @override
+  _ExaminationsPageState createState() => _ExaminationsPageState();
+}
+
+class _ExaminationsPageState extends State<ExaminationsPage> {
+  DateTime _selectedDay = DateTime.now();
+  DateTime _focusedDay = DateTime.now();
+  List<Event> _selectedEvents = [];
+
+  void _onDaySelected(DateTime selectedDay, DateTime focusedDay) {
+    if (!isSameDay(_selectedDay, selectedDay)) {
+      setState(() {
+        _focusedDay = focusedDay;
+        _selectedDay = selectedDay;
+        _selectedEvents = _getEventsForDay(selectedDay);
+      });
+    }
+  }
+
+  Widget _buildEventList(List<Event> events) {
+    return ListView.builder(
+      itemCount: events.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text(events[index].title),
+          subtitle: Text(events[index].date.toString()), // Customize as needed
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -55,12 +107,50 @@ class ExaminationsPage extends StatelessWidget {
                 startingDayOfWeek: StartingDayOfWeek.monday,
                 shouldFillViewport: true,
 
+                headerStyle: const HeaderStyle(
+                  // Customize the header as needed
+                  titleCentered: true,
+                  titleTextStyle: TextStyle(fontSize: 18),
+                  formatButtonVisible: false, // Hide the "2 weeks" button
+                ),
+                selectedDayPredicate: (day) {
+                  return isSameDay(_selectedDay, day);
+                },
+                onDaySelected: (selectedDay, focusedDay) {
+                  setState(() {
+                    _selectedDay = selectedDay;
+                    _focusedDay = focusedDay;
+                  });
+                },
+                onPageChanged: (focusedDay) {
+                  _focusedDay = _selectedDay;
+                },
+                eventLoader: (day) {
+                  return _getEventsForDay(day);
+                },
+
                 // Add more properties and customization as needed
               ),
-              const Center(
-                child: Text('Exams'),
+              Column(
+                children: [
+                  Expanded(
+                    child: _buildEventList(_selectedEvents),
+                  ),
+                ],
               ),
+              // const Center(
+              //   child: Text('Exams'),
+              // ),
             ],
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              // Add your desired action here
+              print('Floating Action Button Pressed');
+            },
+            tooltip: 'Adding events to calendar',
+            child: const Icon(Icons.add),
+            backgroundColor: const Color(0xFF157D9E),
           ),
           bottomNavigationBar: const BottomNavigation(),
         ),
